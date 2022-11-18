dbe21e589c22b2657cee5e1da42b1891649be0c8
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2022-11-18 21:35:31 +0800

    Learned rendering
    I learned the basic of react which is rendering

---
+++ FirstReact/index.js
@@ -0,0 +1,10 @@
+function MainContent() {
+  return <h1>I'm learning the basics of React!</h1>;
+}
+
+ReactDOM.render(
+  <div>
+    <MainContent />
+  </div>,
+  document.getElementById("root")
+);
