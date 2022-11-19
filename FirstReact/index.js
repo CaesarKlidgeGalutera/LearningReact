b6967973a30bae1b0c2db4785755bed7438fa285
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2022-11-19 22:12:32 +0800

    Learned REACT-DOM
    I learned about composability, imperative and declarative

--- FirstReact/index.js
+++ FirstReact/index.js
@@ -1,10 +1,24 @@
-function MainContent() {
-  return <h1>I'm learning the basics of React!</h1>;
-}
-
-ReactDOM.render(
+const myPage = (
   <div>
-    <MainContent />
-  </div>,
-  document.getElementById("root")
+    <img src="React-icon.png" width="40px"></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>My Name is Caesar Klidge C. Galutera</li>
+    </ul>
+  </div>
 );
+
+ReactDOM.render(myPage, document.getElementById("root"));
+
+/*We import React from "react" because the JSX syntax is defined in "react"
+
+
+Declarative - it will know how to do things without telling the details of the process
+
+Imperative - tell the computer step-by-step how to do anything
+
+Composable - It means that you can make small ui and put it in different components to make a build a website.
+ It will also make you website maintainable
+*/
