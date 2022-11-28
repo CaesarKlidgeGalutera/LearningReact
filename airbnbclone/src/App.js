86bc25087733e949a3a278e7984e4eee079b0bd1
Caesar Klidge Galutera <caesarklidgegalutera@gmail.com>
2022-11-28 22:58:19 +0800

    added props on each card component

--- airbnbclone/src/App.js
+++ airbnbclone/src/App.js
@@ -1,12 +1,63 @@
 import "./App.css";
 import { Navbar, Hero, Card } from "./components";
+import image12 from "./components/images/image12.svg";
+import wedding from "./components/images/wedding.svg";
+import mountainbike from "./components/images/mountainbike.svg";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img={image12}
+        rating="5.0"
+        reviewCount="(6)"
+        title="Life lessons with Katie Zafares"
+        price="$136"
+      />
+      <Card
+        img={wedding}
+        rating="5.0"
+        reviewCount="(30)"
+        title="Learn wedding photography"
+        price="$125"
+      />
+      <Card
+        img={mountainbike}
+        rating="4.8"
+        reviewCount="(2)"
+        title="Group Mountain Biking"
+        price="$50"
+      />
+      <Card
+        img={image12}
+        rating="5.0"
+        reviewCount="(6)"
+        title="Life lessons with Katie Zafares"
+        price="$136"
+      />
+      <Card
+        img={wedding}
+        rating="5.0"
+        reviewCount="(30)"
+        title="Learn wedding photography"
+        price="$125"
+      />
+      <Card
+        img={mountainbike}
+        rating="4.8"
+        reviewCount="(2)"
+        title="Group Mountain Biking"
+        price="$50"
+      />
+      <Card
+        img={image12}
+        rating="5.0"
+        reviewCount="(6)"
+        title="Life lessons with Katie Zafares"
+        price="$136"
+      />
     </div>
   );
 }
